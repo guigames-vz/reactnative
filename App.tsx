0f622fbf6c2c7e2aac99c41687b9a49117b7e7cd
Guilherme <guisrmacedo@gmail.com>
2026-08-21 12:46:03 -0300

    gameplay

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello monkey</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#13258a',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
